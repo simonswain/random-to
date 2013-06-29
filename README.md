62f83502bdd6c46129c3c6184081032d88f23f52
Simon Swain <simon@modus.com.au>
2013-06-29 13:53:03 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Random-to
+
+```
+var random = require('random-to');
+
+var x = random.from0to(10);
+var y = random.from1to(10);
+var z = random.from0to(-10);
+```
---
+++ test.js
@@ -0,0 +1,7 @@
+var random = require('./index.js');
+
+var x = random.from0to(10);
+var y = random.from1to(10);
+var z = random.from0to(-10);
+
+console.log(x, y, z);
